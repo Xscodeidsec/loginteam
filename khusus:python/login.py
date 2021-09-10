b10f5079ecaac0ef8546e9a21b81c6d52356d4c2
Xscodeidsec <90434251+Xscodeidsec@users.noreply.github.com>
2021-09-10 10:45:21 +0700

    Create login.py

---
+++ khusus:python/login.py
@@ -0,0 +1,11 @@
+#code by xscode id
+import time, os, sys, time
+from platform import system
+print 'Login anggota !!!'
+sandi = raw_input('masukan kata sandi anggota  : ')
+if sandi == 'SXCODEID_SEC':
+    print 'kata sandi anggota cocok ...'
+else:
+    os.system('kata sandi yang dimasukan  salah ' + exit)
+exit = raw_input('masukan lagi :')
+exit
